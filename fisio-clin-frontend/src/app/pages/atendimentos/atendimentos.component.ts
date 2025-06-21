e955a70041ee257b06ac067d2983daa53cef1b30
luizao <luizsantiago280@gmail.com>
2025-06-21 17:06:39 -0300

    :sparkles: feat: implement header and atendimentos components, enhance layout and styles

---
+++ fisio-clin-frontend/src/app/pages/atendimentos/atendimentos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-atendimentos',
+  imports: [HeaderComponent],
+  templateUrl: './atendimentos.component.html',
+  styleUrl: './atendimentos.component.css'
+})
+export class AtendimentosComponent {
+
+}
